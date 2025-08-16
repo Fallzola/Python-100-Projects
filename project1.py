0f5beecd544850fd8fb2652ff8042cb49da7bedd
Arthur Kalbuch <Fallzitos@gmail.com>
2025-08-16 19:14:58 -0300

    project 1 and 2 complete

---
+++ project1.py
@@ -0,0 +1,4 @@
+city = input("Qual cidade você nasceu?\n")
+pet = input("Qual foi o seu primeiro pet\n")
+
+print(f"O nome da sua banda seria: {city} {pet}s! 🔥")
---
+++ project2.py
@@ -0,0 +1,14 @@
+#Welcome to the tip calculator!
+#What was the total bill? $
+#How  much tip would you like to give? 10, 12, or 15?
+#How many people to split the bill?
+#Each person should pay: $40.00
+
+print("Bem vindo a calculadora de gorjeta!")
+bill = input("Qual foi a sua conta total? $")
+tip = input("Quanto você gostaria de dar como gorjeta (porcentagem)? %")
+people = input("Quantas pessoas vão dividir a conta? ")
+
+total = (float(bill) + (float(bill) * (float(tip) / 100)) / float(people))
+
+print(f"Cada pessoa deve pagar: {total:.2f}$")
